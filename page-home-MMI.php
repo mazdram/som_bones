1d7b23c4043eb982b749893af5c47fcda7c3b143
mazdram <mazdram@health.missouri.edu>
2014-09-22 13:55:22 -0500

    First _base.scss compile, color variables

--- page-home-MMI.php
+++ page-home-MMI.php
@@ -1,10 +1,22 @@
+<?php
+/*
+ Template Name: Home - MMI
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
 <?php get_header(); ?>
 
-			<div class="breadcrumb_wrp">						
-<?php the_breadcrumb(); ?>
-</div>
-
 			<div id="content">
+
 
 				<div id="inner-content" class="wrap cf">
 
@@ -16,10 +28,10 @@
 
 								<header class="article-header">
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+									<h1 class="page-title"><?php the_title(); ?></h1>
 
 
-								</header> <?php // end article header ?>
+								</header>
 
 								<section class="entry-content cf" itemprop="articleBody">
 									<?php
@@ -45,26 +57,30 @@
 											'link_after'  => '</span>',
 										) );
 									?>
-								</section> <?php // end article section ?>
+								</section>
 
-								<footer class="article-footer cf">
+
+								<footer class="article-footer">
+
+                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 								</footer>
 
+								
 
 							</article>
 
 							<?php endwhile; else : ?>
 
 									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
 										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
 										</footer>
 									</article>
 
@@ -77,5 +93,6 @@
 				</div>
 
 			</div>
+
 
 <?php get_footer(); ?>
